652308edfe30a90203d10197e7e5aa897bf689f2
f4d1lah24 <115441731+f4d1lah24@users.noreply.github.com>
2022-10-13 08:00:16 +0700

    Add files via upload

---
+++ python10-tkj4.py
@@ -0,0 +1,47 @@
+# membuat tipe data koleksi
+
+koleksi_data_str = ["pisang","mangga","jambu","semangka"]
+
+koleksi_data_int = [200,300,400,500]
+
+koleksi_data_mix = ["rizky billar",100,True,"reza arab"]
+
+print("koleksi data string:",koleksi_data_str)
+
+print("koleksi data integer:",koleksi_data_int)
+
+print("koleksi data campuran:",koleksi_data_mix)
+
+koleksi_nama_hewan = ["gajah","harimau","singa"]
+
+koleksi_nilai_uts = [77,89,90]
+
+koleksi_teman_sebangku= ["pai","faisal","santo"]
+
+print("koleksi nama hewan :",koleksi_nama_hewan)
+
+print("koleksi nilai uts:",koleksi_nilai_uts)
+
+print("koleksi teman sebangku:",koleksi_teman_sebangku)
+
+#Tampilkan data koleksi dengan indeks
+
+# Data ke 2 nama hewan
+print("nama hewan data ke-2:", koleksi_nama_hewan[1])
+
+# Data pertama nilai uts
+print("nilai uts data ke-1:", koleksi_nilai_uts[0])
+
+# Data terakhir teman sebangku
+print("nama teman sebangku data ke-3:", koleksi_teman_sebangku[2])
+
+# Tambahkan 1 data disetiap data koleksi
+'''tulis disini'''
+
+# ubalah data terakhir nilai uts
+'''tulis disini'''
+
+# ubalah data ke-3 nama hewan
+koleksi_nama_hewan[2]="megalodon"
+
+print(koleksi_nama_hewan)
