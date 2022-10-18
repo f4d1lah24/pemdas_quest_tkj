1c865d755c9364764f1b71211d7844b959ac6ab5
f4d1lah24 <115441731+f4d1lah24@users.noreply.github.com>
2022-10-18 09:56:31 +0700

    Add files via upload

---
+++ python11-tkj4.py
@@ -0,0 +1,19 @@
+#general
+gender=("laki-laki","perempuan")
+
+#menggunakan fungsi tuple,tuple()
+tuple_name=tuple(["jayabaya","lebron james"])
+
+#tuple tunggal
+tuple_tunggal=("arya",)
+
+gabungan=gender+tuple_name
+print(gabungan)
+
+#buatlah 3 koleksi tuple dengan minimal 4 data disetiap koleksi
+
+nama=("acikiwir","aselole","selamet kopling","reza kecap")
+
+hewan=("beruang","singa","kucing","harimau")
+
+top_selingkuh=("rizky billar","reza arab","ahmad dhani","gisella anastasia")
